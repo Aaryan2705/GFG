e2e9eb90dec60988f32c83e507f9801ddb42311a
Aaryan Kumar <119968129+Aaryan2705@users.noreply.github.com>
2024-03-09 15:58:04 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Reverse a String/reverse-a-string.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+    public:
+    string reverseWord(string str)
+    {
+        string s ;
+        int len = str.size();
+        for (int i=len-1;i>=0;i--) {
+            s.push_back(str[i]);
+        }
+        return s ;   
+        
+        
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+    	string s;
+    	cin >> s;
+    	Solution ob;
+    	cout << ob.reverseWord(s) << endl;
+	}
+	return 0;
+	
+}
+
+
+// } Driver Code Ends
