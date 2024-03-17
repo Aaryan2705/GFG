d2f287d1f6f9bde8ef33bb808c0b0fe100fb372c
Aaryan Kumar <119968129+Aaryan2705@users.noreply.github.com>
2024-03-17 15:05:35 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count Pairs whose sum is equal to X/count-pairs-whose-sum-is-equal-to-x.cpp
@@ -0,0 +1,110 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void append(struct Node** headRef, struct Node** tailRef, int newData) {
+    struct Node* new_node = new Node(newData);
+
+    if (*headRef == NULL) {
+        *headRef = new_node;
+    }
+
+    else {
+        (*tailRef)->next = new_node;
+    }
+    *tailRef = new_node;
+}
+
+void printList(struct Node* head) {
+    while (head) {
+        cout << head->data << ' ';
+        head = head->next;
+    }
+}
+
+
+// } Driver Code Ends
+/*
+Structure of the node of the linked list is as
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution{
+  public:
+    // your task is to complete this function
+     int countPairs(struct Node* head1, struct Node* head2, int x) {
+           unordered_map<int,int> a;
+           vector<int> b;
+           struct Node* t1=head1;
+           struct Node* t2=head2;
+           
+           while(t1!=NULL){
+              a[t1->data]++;
+               t1=t1->next;
+           }
+           while(t2!=NULL){
+                    b.push_back(t2->data);
+               t2=t2->next;
+           }
+          
+           int ans=0;
+           
+          for(int i=0;i<b.size();i++){
+              
+              
+              if(a.find(x-b[i])!=a.end()){
+                  ans++;
+              }
+          }
+          return ans;
+           
+    }
+
+
+};
+
+//{ Driver Code Starts.
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        struct Node* head1 = NULL;
+        struct Node* tail1 = NULL;
+        struct Node* tail2 = NULL;
+        struct Node* head2 = NULL;
+        int n1, n2, tmp, x;
+        cin >> n1;
+        while (n1--) {
+            cin >> tmp;
+            append(&head1, &tail1, tmp);
+        }
+        cin >> n2;
+        while (n2--) {
+            cin >> tmp;
+            append(&head2, &tail2, tmp);
+        }
+        cin >> x;
+        Solution obj;
+        cout << obj.countPairs(head1, head2, x) << '\n';
+    }
+    return 0;
+}
+// } Driver Code Ends
