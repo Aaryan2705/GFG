20e9a96048cec54a4c07df803d81581d7b7fc9f1
Aaryan Kumar <119968129+Aaryan2705@users.noreply.github.com>
2024-06-02 09:20:01 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Construct list using given q XOR queries/construct-list-using-given-q-xor-queries.cpp
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+class Matrix {
+  public:
+    template <class T>
+    static void input(vector<vector<T>> &A, int n, int m) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                scanf("%d ", &A[i][j]);
+            }
+        }
+    }
+
+    template <class T>
+    static void print(vector<vector<T>> &A) {
+        for (int i = 0; i < A.size(); i++) {
+            for (int j = 0; j < A[i].size(); j++) {
+                cout << A[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+class Array {
+  public:
+    template <class T>
+    static void input(vector<T> &A, int n) {
+        for (int i = 0; i < n; i++) {
+            scanf("%d ", &A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A) {
+        for (int i = 0; i < A.size(); i++) {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    vector<int> constructList(int q, vector<vector<int>> &queries) {
+        // code here
+         int val=0;
+        vector<int>ans;
+        for(int i=q-1;i>=0;i--)
+        {
+            int x=queries[i][1];
+            if(queries[i][0]==0) {x^=val; ans.push_back(x);}
+            else val^=x;
+        }
+        ans.push_back(val);
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+
+        int q;
+        scanf("%d", &q);
+
+        vector<vector<int>> queries(q, vector<int>(2));
+        Matrix::input(queries, q, 2);
+
+        Solution obj;
+        vector<int> res = obj.constructList(q, queries);
+
+        Array::print(res);
+    }
+}
+
+// } Driver Code Ends
