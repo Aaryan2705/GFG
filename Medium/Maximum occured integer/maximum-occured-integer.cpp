257391a4239b6a7fa45657ea74d260c74409c2cf
Aaryan Kumar <119968129+Aaryan2705@users.noreply.github.com>
2024-06-07 07:37:20 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Maximum occured integer/maximum-occured-integer.cpp
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // l and r are input array
+    // maxx : maximum in r[]
+    // n: size of array
+    // arr[] : declared globally with size equal to maximum in l[] and r[]
+    // Function to find the maximum occurred integer in all ranges.
+    int maxOccured(int n, int l[], int r[], int maxx) {
+        int count[maxx+1]={0};
+        for(int i=0; i<n; i++){
+            count[l[i]]++;
+            count[r[i]+1]--;
+        }
+        int max =0;
+        int maxCount =0 ;
+        for(int i=1; i<maxx; i++){
+            count[i]+=count[i-1];
+            if(maxCount<count[i]){ 
+            max = i;
+            maxCount = count[i];
+            }
+        }
+        return max;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+
+    // taking testcases
+    cin >> t;
+
+    while (t--) {
+        int n;
+
+        // taking size of array
+        cin >> n;
+        int l[n];
+        int r[n];
+
+        // adding elements to array L
+        for (int i = 0; i < n; i++) {
+            cin >> l[i];
+        }
+
+        int maxx = 0;
+
+        // adding elements to array R
+        for (int i = 0; i < n; i++) {
+
+            cin >> r[i];
+            if (r[i] > maxx) {
+                maxx = r[i];
+            }
+        }
+        Solution ob;
+
+        // calling maxOccured() function
+        cout << ob.maxOccured(n, l, r, maxx) << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
