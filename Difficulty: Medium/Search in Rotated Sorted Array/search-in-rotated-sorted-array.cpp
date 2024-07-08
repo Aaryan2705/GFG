cd5a5b3eb64e1ee5b723af51307de43d4af1fc7d
Aaryan Kumar <119968129+Aaryan2705@users.noreply.github.com>
2024-07-08 15:31:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int search(vector<int>& arr, int key) {
+        return (find(arr.begin(),arr.end(),key)!=arr.end()) ? find(arr.begin(),arr.end(),key)-arr.begin(): -1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        cin.ignore();
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int key;
+        cin >> key;
+        Solution ob;
+        cout << ob.search(arr, key) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
