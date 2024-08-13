9c1dfdd73730d36e7e6667babe368721b00e3106
Aaryan Kumar <119968129+Aaryan2705@users.noreply.github.com>
2024-08-13 10:44:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
@@ -7,26 +7,13 @@ using namespace std;
 class Solution {
   public:
     int kthElement(int k, vector<int>& arr1, vector<int>& arr2) {
-        vector<int> merged;
-        int i = 0 , j = 0 ;
-        while(i< arr1.size() && j < arr2.size()){
-            if(arr1[i]<arr2[j]){
-                merged.push_back(arr1[i]);
-                i++;
-            }else {
-                merged.push_back(arr2[j]);
-                j++;
-            }
+        for(auto it: arr2)
+        {
+            arr1.push_back(it);
         }
-        while(i<arr1.size()){
-            merged.push_back(arr1[i]);
-            i++;
-        }
-        while(j<arr2.size()){
-            merged.push_back(arr2[j]);
-            j++;
-        }
-        return merged[k-1];
+        sort(arr1.begin(),arr1.end());
+        
+        return arr1[k-1];
     }
 };
 
