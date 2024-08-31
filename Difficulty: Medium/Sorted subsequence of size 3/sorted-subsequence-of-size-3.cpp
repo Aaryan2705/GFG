e33ea3a89645556a36b12689acebdb11978d11d8
Aaryan Kumar <119968129+Aaryan2705@users.noreply.github.com>
2024-08-31 06:29:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sorted subsequence of size 3/sorted-subsequence-of-size-3.cpp
@@ -0,0 +1,89 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool isSubSequence(vector<int> &v1, vector<int> &v2) {
+    int m = v2.size();
+    int n = v1.size();
+    int j = 0; // For index of v2
+
+    // Traverse v1 and v2
+    for (int i = 0; i < n && j < m; i++) {
+        if (v1[i] == v2[j]) {
+            j++;
+        }
+    }
+    return (j == m);
+}
+
+
+// } Driver Code Ends
+// Function to find three numbers in the given array
+// such that arr[smaller[i]] < arr[i] < arr[greater[i]]
+
+class Solution {
+  public:
+    vector<int> find3Numbers(vector<int> &arr) {
+        // Code here
+        int n=arr.size();
+        
+        vector<int> l(n,INT_MAX);
+        l[0]=arr[0];
+        for(int i=1;i<n;i++){
+            l[i]=min(arr[i],l[i-1]);
+        }
+        
+        vector<int> r(n,INT_MIN);
+        r[n-1]=arr[n-1];
+        for(int i=n-2;i>=0;i--){
+            r[i]=max(arr[i],r[i+1]);
+        }
+            
+        for(int i=0;i<n;i++){
+            if(arr[i]>l[i] && arr[i]<r[i])
+                return {l[i],arr[i],r[i]};
+        }
+        return {};
+        
+    }
+
+
+};
+
+//{ Driver Code Starts.
+
+// Driver program to test above function
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int n = arr.size();
+        Solution obj;
+        auto res = obj.find3Numbers(arr);
+        // wrong format output
+        if (!res.empty() and res.size() != 3) {
+            cout << -1 << "\n";
+        }
+
+        if (res.empty()) {
+            cout << 0 << "\n";
+        } else if ((res[0] < res[1] and res[1] < res[2]) and isSubSequence(arr, res)) {
+            cout << 1 << "\n";
+        } else {
+            cout << -1 << "\n";
+        }
+    }
+
+    return 0;
+}
+// } Driver Code Ends
