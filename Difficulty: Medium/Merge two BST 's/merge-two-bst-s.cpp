2f3e15f16f601152d3e4c092a7ea1f8c600072fe
Aaryan Kumar <119968129+Aaryan2705@users.noreply.github.com>
2024-09-30 19:52:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge two BST 's/merge-two-bst-s.cpp
+++ Difficulty: Medium/Merge two BST 's/merge-two-bst-s.cpp
@@ -94,58 +94,40 @@ struct Node {
 */
 class Solution {
   public:
-  
-    // code to perforn in-order traversal of BSTs and form two sorted arrays
-    
-    void inOrderTraversal(Node* root, vector<int>& ans) {
-        stack<Node*> s;
-        Node* temp = root;
-    
-        while (!s.empty() || temp!=NULL) {
-            while (temp!=NULL) {
-                s.push(temp);
-                temp = temp->left;
-        }
-        temp = s.top();
-        s.pop();
-        ans.push_back(temp->data);
-        temp = temp->right;
-    }
-}
-    // code to merge two sorted vector using merge sort
-    
-    vector<int> MergeSortedVectors(vector<int>& v1,vector<int>& v2) {
-        vector<int> mergedArr;
-        int i = 0, j = 0;
-        while (i < v1.size() && j < v2.size()) {
-            if (v1[i] < v2[j]) {
-                mergedArr.push_back(v1[i]);
-                i++;
-            } 
-            else {
-                mergedArr.push_back(v2[j]);
-                j++;
-            }
-        }
-        while (i < v1.size()) {
-            mergedArr.push_back(v1[i]);
-            i++;
-        }
-        while (j < v2.size()) {
-            mergedArr.push_back(v2[j]);
-            j++;
-        }
-        return mergedArr;
-    }
+
+  void r(Node *root, vector<int>&v){
+
+      if(root==nullptr) return;
+
+      v.push_back(root->data);
+
+      r(root->left,v);
+
+      r(root->right,v);
+
+  }
+
+    // Function to return a list of integers denoting the node
+
+    // values of both the BST in a sorted order.
 
     vector<int> merge(Node *root1, Node *root2) {
-        vector<int> v1, v2;
-        inOrderTraversal(root1, v1);
-        inOrderTraversal(root2, v2);
-        return MergeSortedVectors(v1, v2);
-    }
-};
 
+        // Your code here
+
+        vector<int>ans;
+
+        r(root1,ans);
+
+        r(root2,ans);
+
+        sort(ans.begin(),ans.end());
+
+        return ans;
+
+    }
+
+};
 
 //{ Driver Code Starts.
 int main() {
