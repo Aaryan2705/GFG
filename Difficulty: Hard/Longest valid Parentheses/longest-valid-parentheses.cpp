238af0d831758af18fc7edb6675455f1aa4f31b8
Aaryan Kumar <119968129+Aaryan2705@users.noreply.github.com>
2025-02-22 10:35:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Longest valid Parentheses/longest-valid-parentheses.cpp
+++ Difficulty: Hard/Longest valid Parentheses/longest-valid-parentheses.cpp
@@ -6,52 +6,25 @@ using namespace std;
 
 
 // } Driver Code Ends
-// User function Template for C++
 
 class Solution {
   public:
     int maxLength(string& s) {
-        int n = s.size();
-        // code here
-        stack<int>st; 
-        int i = 0;
-        vector<int>arr(n, 0);
-        while(i < n){
-            if(s[i] == '('){
+        stack<int>st;
+        st.push(-1);
+        int ans=0;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='(')
+            st.push(i);
+            else{
+                st.pop();
+                if(st.empty())
                 st.push(i);
+                else
+                ans=max(ans,i-st.top());
             }
-            else{
-                if(st.empty()){
-                    arr[i] = -1;
-                }
-                else{
-                    st.pop();
-                }
-                
-            }
-            i++;
         }
-        
-        while(!st.empty()){
-            arr[st.top()] = -1;
-            st.pop();
-        }
-                
-        int ans = 0;
-        i = 0;
-        int l = -1;
-        while(i < n){
-            if(arr[i] == -1){
-                l = i;
-            }
-            else{
-                ans = max(ans, i - l);
-            }
-            i++;
-        }
-         
         return ans;
-        
     }
 };
 
@@ -66,6 +39,9 @@ int main() {
 
         Solution ob;
         cout << ob.maxLength(S) << "\n";
+
+        cout << "~"
+             << "\n";
     }
     return 0;
 }
