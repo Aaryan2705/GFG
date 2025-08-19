cf4dc8d8d983f6818c95b3675ea2602eca187b2b
Aaryan Kumar <119968129+Aaryan2705@users.noreply.github.com>
2025-08-19 13:34:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Farthest Smaller Right/farthest-smaller-right.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    vector<int> farMin(vector<int>& arr) {
+        const int n = arr.size();
+        vector<int> ans(n, -1);
+        priority_queue<pair<int, int>> pq;
+        
+        for (int i = 0; i < n - 1; i++) {
+            pq.push({arr[i], i});
+        }
+        
+        for (int i = n - 1; i >= 0; i--) {
+            while (!pq.empty() && pq.top().first > arr[i]) {
+                if (pq.top().second < i) ans[pq.top().second] = i;
+                pq.pop();
+            }
+        }
+        
+        return ans;
+    }
+};
